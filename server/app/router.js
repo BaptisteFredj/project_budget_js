10acdfb3d4570bc5d54496a28b14cd7fee1343f8
BaptisteFredj <redj2304@gmail.com>
2024-09-06 11:34:12 +0200

    feat: user CRUD

--- server/app/router.js
+++ server/app/router.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Import And Use Routers Here
-/* ************************************************************************* */
+const user = require("./controllers/userActions");
 
-/* ************************************************************************* */
+// User routes
+router.get("/users", user.browse);
+router.get("/user/:id", user.read);
+router.post("/user", user.add);
+router.put("/user/:id", user.edit);
+router.delete("/user/:id", user.destroy);
 
 module.exports = router;
