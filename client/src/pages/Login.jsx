5e3179f9b550ed7365f52da2cb738311e97f3ca9
BaptisteFredj <redj2304@gmail.com>
2024-09-11 09:44:46 +0200

    fix: removed dead code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,21 +3,17 @@ import { useNavigate, useOutletContext } from "react-router-dom";
 import { login } from "../services/request";
 
 function Login() {
-  // Références pour les champs email et mot de passe
   const emailRef = useRef();
   const passwordRef = useRef();
 
   const { setUser } = useOutletContext();
 
-  // Hook pour la navigation
   const navigate = useNavigate();
 
-  // Gestionnaire de soumission du formulaire
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      // Appel à l'API pour demander une connexion
       const user = await login(
         emailRef.current.value,
         passwordRef.current.value
@@ -26,12 +22,10 @@ function Login() {
       setUser(user);
       navigate(`/transactions`);
     } catch (err) {
-      // Log des erreurs possibles
       console.error(err);
     }
   };
 
-  // Rendu du composant formulaire
   return (
     <form onSubmit={handleSubmit}>
       <div>
