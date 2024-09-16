25f7ff558e566cbe0b21322aca0cde373661de12
BaptisteFredj <redj2304@gmail.com>
2024-09-16 18:02:58 +0200

    feat: add a button to access category edit page

--- client/src/pages/CategoryDetails.jsx
+++ client/src/pages/CategoryDetails.jsx
@@ -1,13 +1,16 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 
 function CategoryDetails() {
   const { category } = useLoaderData();
 
   return (
-    <ul>
-      <li>Nom de la catégorie : {category.name}</li>
-      <li>Icône : {category.icon}</li>
-    </ul>
+    <>
+      <ul>
+        <li>Nom de la catégorie : {category.name}</li>
+        <li>Icône : {category.icon}</li>
+      </ul>
+      <Link to={`/categories/${category.id}/edit`}>Modifier</Link>
+    </>
   );
 }
 
