8f5010796ec9a42f52ae7750d74a2d8f56800e5f
BaptisteFredj <redj2304@gmail.com>
2024-09-17 18:43:54 +0200

    fix: removed useless useState from category form

--- client/src/pages/CategoryForm.jsx
+++ client/src/pages/CategoryForm.jsx
@@ -1,17 +1,6 @@
-import { useState } from "react";
 import { Form } from "react-router-dom";
 
 function CategoryForm() {
-  const [name, setName] = useState("");
-  const [icon, setIcon] = useState("");
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-  const handleIconChange = (event) => {
-    setIcon(event.target.value);
-  };
-
   return (
     <>
       <h1>Créer une catégorie</h1>
@@ -22,18 +11,9 @@ function CategoryForm() {
           id="name"
           name="name"
           placeholder="Nom de de la catégorie"
-          value={name}
-          onChange={handleNameChange}
         />
         <label htmlFor="icon">Icone de la catégorie</label>
-        <input
-          type="text"
-          id="icon"
-          name="icon"
-          placeholder="Icône"
-          value={icon}
-          onChange={handleIconChange}
-        />
+        <input type="text" id="icon" name="icon" placeholder="Icône" />
         <button type="submit">Ajouter</button>
       </Form>
     </>
