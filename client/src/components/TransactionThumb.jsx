910dd728f9c97bd1446ae6f8a4ac961f10eb85dd
BaptisteFredj <redj2304@gmail.com>
2024-09-17 17:52:19 +0200

    fix: add brackets to some imports for consistency

--- client/src/components/TransactionThumb.jsx
+++ client/src/components/TransactionThumb.jsx
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import { PropTypes } from "prop-types";
 import frenchType from "../utils/functions";
 
 export default function TransactionThumb({ transaction }) {
