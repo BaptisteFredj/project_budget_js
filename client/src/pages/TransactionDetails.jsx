6808bf62ee815eacb4fe5560b95df99357194fc9
BaptisteFredj <redj2304@gmail.com>
2024-10-11 10:25:16 +0200

    fix: removed duplicated line

--- client/src/pages/TransactionDetails.jsx
+++ client/src/pages/TransactionDetails.jsx
@@ -7,9 +7,8 @@ function TransactionDetails() {
   return (
     <>
       <ul>
-        <li>Nom de la transaction : {transaction.name}</li>
-        <li>Montant de la transaction : {transaction.amount}</li>
         <li>Type de transaction : {frenchType(transaction.type)}</li>
+        <li>Montant de la transaction : {transaction.amount}</li>
         <li>Date de la transaction : {transaction.date}</li>
         <li>Type de transaction : {transaction.type}</li>
         <li>Catégorie de la transaction : {transaction.category_name}</li>
