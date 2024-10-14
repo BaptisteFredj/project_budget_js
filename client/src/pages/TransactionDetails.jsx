932052ed5a27ab2116757509bd5032d3885df88a
BaptisteFredj <redj2304@gmail.com>
2024-10-14 11:34:26 +0200

    refactor: duplicated line deleted

--- client/src/pages/TransactionDetails.jsx
+++ client/src/pages/TransactionDetails.jsx
@@ -11,7 +11,6 @@ function TransactionDetails() {
         <li>Montant de la transaction : {transaction.amount}</li>
         <li>Type de transaction : {frenchType(transaction.type)}</li>
         <li>Date de la transaction : {transaction.date}</li>
-        <li>Type de transaction : {transaction.type}</li>
         <li>Catégorie de la transaction : {transaction.category_name}</li>
       </ul>
       <Link to={`/transactions/${transaction.id}/edit`}>Modifier</Link>
