16126195b6ff586351b26377dc348cb0cec723d0
BaptisteFredj <redj2304@gmail.com>
2024-10-15 15:48:29 +0200

    fix: improved schema sql logic add user_id to transaction table removed not null from PK tuples

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,53 +1,32 @@
-create table user (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  create table user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     hashed_password VARCHAR(255) NOT NULL,
     created_at DATE
   );
-  
-  INSERT INTO user (email, hashed_password, created_at) VALUES
-  ('john@example.com', 'nothashed', '2023-01-15'),
-  ('jane@example.com', 'nothashed', '2023-02-10'),
-  ('alice@example.com', 'nothashed', '2023-03-20');
-  
-  
+   
   create table category (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     icon VARCHAR(255),
     user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES user(id)
   );
   
-  INSERT INTO category (name, icon, user_id) VALUES
-  ('Groceries', 'groceries_icon.png', 1),
-  ('Rent', 'rent_icon.png', 1),
-  ('Entertainment', 'entertainment_icon.png', 2),
-  ('Utilities', 'utilities_icon.png', 2),
-  ('Travel', 'travel_icon.png', 3);
-  
-  
   create table transaction (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     date DATE,
     amount INT,
     type VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
     category_id INT,
+    FOREIGN KEY(user_id) REFERENCES user(id),
     FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
   );
   
-  INSERT INTO transaction (name, date, amount, type, category_id) VALUES
-  ('kebab', '2023-08-01', 50, 'expense', 1),
-  ('mcdo', '2023-08-05', 1200, 'expense', 2),
-  ('bk', '2023-08-12', 300, 'expense', 3),
-  ('kfc', '2023-08-15', 80, 'expense', 4),
-  ('dominos', '2023-08-20', 200, 'expense', 5),
-  ('otacos', '2023-08-22', 1500, 'income', 2);
-  
-  
   create table budget (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     amount INT,
     start_date DATE,
@@ -57,11 +36,3 @@ create table user (
     FOREIGN KEY (user_id) REFERENCES user(id),
     FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
   );
-  
-  INSERT INTO budget (name, amount, start_date, end_date, category_id, user_id) VALUES
-  ('course', 500, '2023-09-01', '2023-09-30', 1, 1),
-  ('bar', 1500, '2023-09-01', '2023-09-30', 2, 1),
-  ('vélo', 300, '2023-09-01', '2023-09-30', 3, 2),
-  ('piscine', 400, '2023-09-01', '2023-09-30', 4, 2),
-  ('restaurant', 1000, '2023-09-01', '2023-09-30', 5, 3);
-  
