de5d6719090a4c71f72697f43f9a2798aabd3f1d
BaptisteFredj <redj2304@gmail.com>
2024-10-17 15:25:51 +0200

    feat: budget form data checked with middleware

--- server/app/services/dataValidator.js
+++ server/app/services/dataValidator.js
@@ -1,41 +1,31 @@
 const budgetFormValidator = async (req, res, next) => {
   const formData = req.body;
-  //   const specialCharacters = /[^a-zA-Z0-9 ]/g;
+  const specialCharacters = /[^a-zA-Z0-9 ]/g;
 
   try {
     if (formData.name.length > 100) {
       throw new Error("Nom trop long : maximum 100 caractères.");
     }
-
-    // if (formData.name.match(specialCharacters)) {
-    //   return res.json({
-    //     message: "Seulement les caractères alphanumériques sont autorisés.",
-    //   });
-    // }
-
-    // if (
-    //   !Number.isInteger(Number(formData.amount)) ||
-    //   Number(formData.amount) < 0
-    // ) {
-    //   return res.json({
-    //     message: "Seul les nombres entiers positifs sont acceptés.",
-    //   });
-    // }
+    if (formData.name.match(specialCharacters)) {
+      throw new Error(
+        "Seulement les caractères alphanumériques sont autorisés."
+      );
+    }
+    // At first, i add a "or" condition to check if input is an integer, but the form already does it
+    if (Number(formData.amount) <= 0) {
+      throw new Error("Seul les nombres entiers positifs sont acceptés.");
+    }
+    if (new Date(formData.start_date) > new Date(formData.end_date)) {
+      throw new Error(
+        "La date de démarrage d'un budget doit forcément être avant sa date de fin."
+      );
+    }
 
     return next();
   } catch (error) {
     return res.json({ message: error.message });
   }
 };
-
-// {
-//   name: "Bar",
-//   amount: 300,
-//   start_date: "2024-10-18",
-//   end_date: "2024-10-18",
-//   category_id: null,
-//   user_id: 1,
-// };
 
 module.exports = {
   budgetFormValidator,
