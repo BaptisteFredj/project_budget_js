06cb0593aa3a5baad3af7773dcb0ac46bdbbfe35
BaptisteFredj <redj2304@gmail.com>
2024-10-22 17:03:22 +0200

    refactor: add several validation steps simplified regex now allows name between 3 to 100 characters trim to forbid blank spaces outside characters

--- client/src/utils/functions.js
+++ client/src/utils/functions.js
@@ -17,17 +17,24 @@ export function toIso(europeanDate) {
 
 export function budgetFormValidator(formData) {
   const errors = {};
-  const specialCharacters = /[^\p{L}0-9 ]/gu;
+  const specialCharacters = /[^A-Za-zÀ-ÿ0-9 ]/;
+  const trimmedName = formData.name.trim();
 
-  if (formData.name.length > 100) {
-    errors.NameError = "Nom trop long : maximum 100 caractères.";
+  if (!trimmedName.length) {
+    errors.NameError =
+      "Le nom ne peut pas être vide ou composé uniquement d'espaces. Les espaces en début et fin de nom ne comptent pas comme un caractère.";
+  }
+  if (trimmedName) {
+    if (trimmedName.length > 100) {
+      errors.NameError = "Nom trop long : maximum 100 caractères.";
+    }
+    if (trimmedName.length < 3) {
+      errors.NameError =
+        "Nom trop court : 3 caractères minimum. Les espaces en début et fin de nom ne comptent pas comme un caractère.";
+    }
   }
 
-  if (formData.name.length < 3) {
-    errors.NameError = "Nom trop court : 3 caractères minimum.";
-  }
-
-  if (formData.name.match(specialCharacters)) {
+  if (trimmedName.match(specialCharacters)) {
     errors.CharacterError =
       "Seulement les caractères alphanumériques sont autorisés.";
   }
