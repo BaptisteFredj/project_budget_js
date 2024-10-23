07709df7efef7d6687f1c9e80d018176777dab9f
BaptisteFredj <redj2304@gmail.com>
2024-10-23 19:20:31 +0200

    refactor: add icon table to handle icons

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -4,12 +4,18 @@
     hashed_password VARCHAR(255) NOT NULL,
     created_at DATE
   );
+
+  create table icon (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    path VARCHAR(255)
+  );
    
   create table category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
-    icon VARCHAR(255),
+    icon_id INT DEFAULT 1,
     user_id INT NOT NULL,
+    FOREIGN KEY(icon_id) REFERENCES icon(id),
     FOREIGN KEY(user_id) REFERENCES user(id)
   );
   
@@ -36,3 +42,33 @@
     FOREIGN KEY (user_id) REFERENCES user(id),
     FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
   );
+
+INSERT INTO icon (path) VALUES 
+    ('/assets/icons/question.svg'),
+    ('/assets/icons/arrows1.svg'),
+    ('/assets/icons/arrows2.svg'),
+    ('/assets/icons/baby.svg'),
+    ('/assets/icons/bar1.svg'),
+    ('/assets/icons/bar2.svg'),
+    ('/assets/icons/bitcoin.svg'),
+    ('/assets/icons/cloth1.svg'),
+    ('/assets/icons/cloth2.svg'),
+    ('/assets/icons/controller.svg'),
+    ('/assets/icons/dollar.svg'),
+    ('/assets/icons/euro.svg'),
+    ('/assets/icons/filelist.txt'),
+    ('/assets/icons/food1.svg'),
+    ('/assets/icons/food2.svg'),
+    ('/assets/icons/gift.svg'),
+    ('/assets/icons/grocery1.svg'),
+    ('/assets/icons/grocery2.svg'),
+    ('/assets/icons/health1.svg'),
+    ('/assets/icons/health2.svg'),
+    ('/assets/icons/heart1.svg'),
+    ('/assets/icons/home.svg'),
+    ('/assets/icons/money1.svg'),
+    ('/assets/icons/school.svg'),
+    ('/assets/icons/sport1.svg'),
+    ('/assets/icons/sport2.svg'),
+    ('/assets/icons/transport.svg'),
+    ('/assets/icons/yen.svg');
