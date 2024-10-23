221102cb231a8a4db751d68e2512bad5b373fbe2
BaptisteFredj <redj2304@gmail.com>
2024-10-23 16:10:05 +0200

    refactor: name validator exported for reusability

--- server/app/services/dataValidator.js
+++ server/app/services/dataValidator.js
@@ -1,39 +1,45 @@
-const budgetFormValidator = async (req, res, next) => {
-  const formData = req.body;
-  const { amount, start_date: startDate, end_date: endDate } = req.body;
+const nameValidator = async (req) => {
+  const { name } = req.body;
   const specialCharacters = /[^A-Za-zÀ-ÿ0-9 ]/;
+  const trimmedName = name.trim();
 
-  try {
-    if (!formData.name) {
+  if (!trimmedName) {
+    const error = new Error(
+      "Le nom ne peut pas être vide ou composé uniquement d'espaces. Les espaces en début et fin de nom ne comptent pas comme un caractère."
+    );
+    error.name = "NameError";
+    throw error;
+  }
+
+  if (trimmedName) {
+    if (trimmedName.length > 100) {
+      const error = new Error("Nom trop long : maximum 100 caractères.");
+      error.name = "NameError";
+      throw error;
+    }
+    if (trimmedName.length < 3) {
       const error = new Error(
-        "Le nom ne peut pas être vide ou composé uniquement d'espaces. Les espaces en début et fin de nom ne comptent pas comme un caractère."
+        "Nom trop court : 3 caractères minimum. Les espaces en début et fin de nom ne comptent pas comme un caractère."
       );
       error.name = "NameError";
       throw error;
     }
+  }
 
-    if (formData.name) {
-      if (formData.name.length > 100) {
-        const error = new Error("Nom trop long : maximum 100 caractères.");
-        error.name = "NameError";
-        throw error;
-      }
-      if (formData.name.length < 3) {
-        const error = new Error(
-          "Nom trop court : 3 caractères minimum. Les espaces en début et fin de nom ne comptent pas comme un caractère."
-        );
-        error.name = "NameError";
-        throw error;
-      }
-    }
+  if (trimmedName.match(specialCharacters)) {
+    const error = new Error(
+      "Seulement les caractères alphanumériques sont autorisés."
+    );
+    error.name = "CharacterError";
+    throw error;
+  }
+};
 
-    if (formData.name.match(specialCharacters)) {
-      const error = new Error(
-        "Seulement les caractères alphanumériques sont autorisés."
-      );
-      error.name = "CharacterError";
-      throw error;
-    }
+const budgetFormValidator = async (req, res, next) => {
+  const { amount, start_date: startDate, end_date: endDate } = req.body;
+
+  try {
+    await nameValidator(req);
 
     // At first, i add a "or" condition to check if input is an integer, but the form already does it
     if (Number(amount) <= 0) {
@@ -51,7 +57,6 @@ const budgetFormValidator = async (req, res, next) => {
       error.name = "DateError";
       throw error;
     }
-
     return next();
   } catch (error) {
     return res.json({ name: error.name, message: error.message });
