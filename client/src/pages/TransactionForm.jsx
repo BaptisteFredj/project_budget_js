a79394fbb42f5c46eba38a55ece0032a7e2a6904
BaptisteFredj <redj2304@gmail.com>
2024-10-30 17:47:23 +0100

    style: minor changes

--- client/src/pages/TransactionForm.jsx
+++ client/src/pages/TransactionForm.jsx
@@ -15,7 +15,7 @@ function TransactionForm() {
 
   return (
     <Form method="post" className="transaction_label_form">
-      <label className="transaction_amount_input" htmlFor="amount">
+      <label className="transaction_amount_label" htmlFor="amount">
         Montant
       </label>
       <input
@@ -24,28 +24,37 @@ function TransactionForm() {
         name="amount"
         placeholder="Exemple : 10 €"
         step="0.01"
+        className="transaction_amount_input"
       />
       {errors?.AmountError}
 
-      <label htmlFor="name">Libellé</label>
+      <label className="transaction_name_label" htmlFor="name">
+        Libellé
+      </label>
       <input
         type="text"
         id="name"
         name="name"
         placeholder="Exemple : Paire de chaussures"
+        className="transaction_name_input"
       />
       {errors?.NameError}
       {errors?.CharacterError}
 
-      <label htmlFor="date">Date</label>
+      <label className="transaction_date_label" htmlFor="date">
+        Date
+      </label>
       <input
         type="date"
         id="date"
         name="date"
         placeholder="Date de la transaction"
+        className="transaction_date_input"
       />
 
-      <label htmlFor="type">Type</label>
+      <label className="transaction_type_label" htmlFor="type">
+        Type
+      </label>
       <div className="type_selector">
         <button
           type="button"
@@ -71,7 +80,9 @@ function TransactionForm() {
       </div>
       <input type="hidden" name="type" value={selectedType} />
 
-      <label htmlFor="category">Catégorie</label>
+      <label className="transaction_category_label" htmlFor="category">
+        Catégorie
+      </label>
       <select id="category" name="category">
         <option value="">Sans catégorie</option>
         {categories.map((category) => (
