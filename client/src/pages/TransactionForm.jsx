776d857b203ac39ff5549da92606b30a9dc8089b
BaptisteFredj <redj2304@gmail.com>
2024-10-30 17:10:35 +0100

    style: base style for transaction form

--- client/src/pages/TransactionForm.jsx
+++ client/src/pages/TransactionForm.jsx
@@ -1,60 +1,63 @@
 import { Form, useLoaderData, useActionData } from "react-router-dom";
 
+import "../assets/styles/transactionform.css";
+
 function TransactionForm() {
   const { categories } = useLoaderData();
   const errors = useActionData();
 
   return (
-    <>
-      <h1>Ajouter une transaction</h1>
-      <Form method="post">
-        <label htmlFor="name">Nom de la transaction</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Nom de la transaction"
-        />
-        {errors?.NameError}
-        {errors?.CharacterError}
+    <Form method="post" className="transaction_label_form">
+      <label className="transaction_amount_input" htmlFor="amount">
+        Montant
+      </label>
+      <input
+        type="number"
+        id="amount"
+        name="amount"
+        placeholder="Exemple : 10 €"
+        step="0.01"
+      />
+      {errors?.AmountError}
 
-        <label htmlFor="amount">Montant de la transaction</label>
-        <input
-          type="number"
-          id="amount"
-          name="amount"
-          placeholder="Montant"
-          step="0.01"
-        />
-        {errors?.AmountError}
+      <label htmlFor="name">Libellé</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Exemple : Paire de chaussures"
+      />
+      {errors?.NameError}
+      {errors?.CharacterError}
 
-        <label htmlFor="date">Date de transaction</label>
-        <input
-          type="date"
-          id="date"
-          name="date"
-          placeholder="Date de la transaction"
-        />
+      <label htmlFor="date">Date</label>
+      <input
+        type="date"
+        id="date"
+        name="date"
+        placeholder="Date de la transaction"
+      />
 
-        <label htmlFor="type">Type de transaction</label>
-        <select id="type" name="type">
-          <option value="income">Revenu</option>
-          <option value="expense">Dépense</option>
-          <option value="transfer">Transfert</option>
-        </select>
+      <label htmlFor="type">Type</label>
+      <select id="type" name="type">
+        <option value="income">Revenu</option>
+        <option value="expense">Dépense</option>
+        <option value="transfer">Transfert</option>
+      </select>
 
-        <label htmlFor="category">Catégorie de transaction</label>
-        <select id="category" name="category">
-          <option value="">Sans catégorie</option>
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-        <button type="submit">Ajouter</button>
-      </Form>
-    </>
+      <label htmlFor="category">Catégorie</label>
+      <select id="category" name="category">
+        <option value="">Sans catégorie</option>
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+      <button className="add_button transaction_form_button" type="submit">
+        Confirmer la transaction
+      </button>
+    </Form>
   );
 }
 
