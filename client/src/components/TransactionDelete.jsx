10f50f2ee48c2513e2396f9837d171ae79b80628
BaptisteFredj <redj2304@gmail.com>
2024-10-31 12:17:34 +0100

    feat: delete popover

--- client/src/components/TransactionDelete.jsx
+++ client/src/components/TransactionDelete.jsx
@@ -1,13 +1,27 @@
 import { Form, Link } from "react-router-dom";
 
+import "../assets/styles/transactiondelete.css";
+
 function TransactionDelete() {
   return (
-    <Form method="delete">
-      <Link to="/transactions/">
-        <button type="button">Annuler</button>
-      </Link>
-      <button type="submit">Supprimer</button>
-    </Form>
+    <section className="warning_popover">
+      <div className="warning_text">
+        <h1 className="warning_h1">Êtes-vous sûr(e) ?</h1>
+        <h2 className="warning_h2">Cette action ne peut être annulée.</h2>
+      </div>
+      <div className="warning_buttons">
+        <Link to="/transactions/">
+          <button className="warning_cancel" type="button">
+            Annuler
+          </button>
+        </Link>
+        <Form method="delete">
+          <button className="warning_confirm" type="submit">
+            Supprimer
+          </button>
+        </Form>
+      </div>
+    </section>
   );
 }
 
