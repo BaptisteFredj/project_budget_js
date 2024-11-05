445f04edb11004886f074e9d2f751562725ee2d5
BaptisteFredj <redj2304@gmail.com>
2024-11-05 16:15:38 +0100

    feat: create and read account models

--- server/database/models/AccountRepository.js
+++ server/database/models/AccountRepository.js
@@ -4,6 +4,26 @@ class AccountRepository extends AbstractRepository {
   constructor() {
     super({ table: "account" });
   }
+
+  async readAccountsByUser(userId) {
+    const [rows] = await this.database.query(
+      ` SELECT a.id id, a.name name, a.amount amount
+        FROM ${this.table} a
+        WHERE user_id = ?`,
+      [userId]
+    );
+    return rows;
+  }
+
+  async create(account) {
+    const [result] = await this.database.query(
+      ` INSERT INTO ${this.table} (name, amount)
+          VALUES (?, ?)`,
+      [account.name, account.amount]
+    );
+
+    return result;
+  }
 }
 
 module.exports = AccountRepository;
