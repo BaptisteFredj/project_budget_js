fd697aab4a0572ca0719f91cf73c4ed8487a992c
BaptisteFredj <redj2304@gmail.com>
2024-11-13 12:17:16 +0100

    feat: initial dashboard layout

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,24 @@
+export default function Dashboard() {
+  return (
+    <>
+      <h2>Dépenses par catégorie</h2>
+      <p>Dépenses sur la période : XXX €</p>
+
+      <h2>Budgets en cours</h2>
+      <ul>
+        <li>Budget 1</li>
+        <li>Budget 2</li>
+        <li>Budget 3</li>
+      </ul>
+
+      <h2>Dernières dépenses</h2>
+      <ul>
+        <li>Dépense 1</li>
+        <li>Dépense 2</li>
+        <li>Dépense 3</li>
+        <li>Dépense 4</li>
+        <li>Dépense 5</li>
+      </ul>
+    </>
+  );
+}
