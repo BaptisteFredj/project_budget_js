8907065a8510c6080d32947954138cbb0aec119c
BaptisteFredj <redj2304@gmail.com>
2024-11-15 16:51:54 +0100

    style: budget form

--- client/src/pages/BudgetForm.jsx
+++ client/src/pages/BudgetForm.jsx
@@ -1,53 +1,70 @@
 import { Form, useActionData, useLoaderData } from "react-router-dom";
 
+import "../assets/styles/budgetform.css";
+
 function BudgetForm() {
   const { categories } = useLoaderData();
   const errors = useActionData();
 
   return (
-    <>
-      <h1>Créer un budget</h1>
-      <Form method="post">
-        <label htmlFor="amount">Montant du budget</label>
-        <input
-          type="number"
-          id="amount"
-          name="amount"
-          placeholder="Montant"
-          required
-        />
-        {errors?.AmountError}
+    <Form method="post" className="budget_label_form">
+      <label className="budget_amount_label" htmlFor="amount">
+        Montant du budget
+      </label>
+      <input
+        className="budget_amount_input"
+        type="number"
+        id="amount"
+        name="amount"
+        placeholder="Montant"
+        required
+      />
+      {errors?.AmountError}
 
-        <label htmlFor="start_date">Date de début du budget</label>
-        <input
-          type="date"
-          id="start_date"
-          name="start_date"
-          placeholder="Date de début du budget"
-          required
-        />
-        {errors?.DateError}
+      <label className="budget_start_date_label" htmlFor="start_date">
+        Date de début du budget
+      </label>
+      <input
+        className="budget_start_date_input"
+        type="date"
+        id="start_date"
+        name="start_date"
+        placeholder="Date de début du budget"
+        required
+      />
+      {errors?.DateError}
 
-        <label htmlFor="end_date">Date de fin du budget</label>
-        <input
-          type="date"
-          id="end_date"
-          name="end_date"
-          placeholder="Date de fin du budget"
-          required
-        />
-        <label htmlFor="category">Catégorie du budget</label>
-        <select id="category" name="category" required>
-          <option value="">Sélectionner une catégorie</option>
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-        <button type="submit">Ajouter</button>
-      </Form>
-    </>
+      <label className="budget_end_date_label" htmlFor="end_date">
+        Date de fin du budget
+      </label>
+      <input
+        className="budget_end_date_input"
+        type="date"
+        id="end_date"
+        name="end_date"
+        placeholder="Date de fin du budget"
+        required
+      />
+      <label className="budget_category_label" htmlFor="category">
+        Catégorie du budget
+      </label>
+      <select
+        className="budget_category_select"
+        id="category"
+        name="category"
+        required
+      >
+        <option value="">Sélectionner une catégorie</option>
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+      <button className="add_button budget_form_button" type="submit">
+        Ajouter
+      </button>
+    </Form>
   );
 }
 
